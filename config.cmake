a653eaf9573d44deebd924e699ae39460ac01936
Nathan Forbes <me@nathanforbes.com>
2011-11-12 10:35:32 -0500

    v0.14: add CMake

---
+++ config.cmake
@@ -0,0 +1,21 @@
+#cmakedefine HAVE_INTTYPES_H 1
+#cmakedefine HAVE_MALLOC 1
+#cmakedefine HAVE_MEMORY_H 1
+#cmakedefine HAVE_MEMSET 1
+#cmakedefine HAVE_MKDIR 1
+#cmakedefine HAVE_REALLOC 1
+#cmakedefine HAVE_STDBOOL_H 1
+#cmakedefine HAVE_STDINT_H 1
+#cmakedefine HAVE_STDLIB_H 1
+#cmakedefine HAVE_STRINGS_H 1
+#cmakedefine HAVE_STRING_H 1
+#cmakedefine HAVE_STRRCHR 1
+#cmakedefine HAVE_SYS_IOCTL_H 1
+#cmakedefine HAVE_SYS_STAT_H 1
+#cmakedefine HAVE_SYS_TYPES_H 1
+#cmakedefine HAVE_UNISTD_H 1
+#cmakedefine malloc
+#cmakedefine realloc
+#cmakedefine size_t
+#define PACKAGE_NAME "bacon"
+#define PACKAGE_VERSION "0.14"
