3a6a7392ee05b741358d7610003ef8597f82dcfc
Nathan Forbes <me@nathanforbes.com>
2013-10-05 23:04:52 -0400

    add copyright notice and #ifdef BACON_USING_GTK to bacon-gtk files

--- bacon-gtk.c
+++ bacon-gtk.c
@@ -1,3 +1,23 @@
+/*
+ * bacon - A command line tool for viewing/downloading CyanogenMod ROMs
+ *         for Android devices.
+ *
+ * Copyright (C) 2013  Nathan Forbes
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifdef BACON_USING_GTK
 #include <string.h>
 
 #include <gtk/gtk.h>
@@ -416,4 +436,5 @@ bacon_gtk_main (int *argc, char ***argv)
       break;
   }
 }
+#endif /* BACON_USING_GTK */
 
